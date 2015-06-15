ab6de90c1d5de6f136366c9d3dd1bf05c2853340
JokerZhu <244912654@qq.com>
2015-06-15 22:24:15 +0800

    打飞机小游戏框架

---
+++ firstGame/dafeiji.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3.4
+'''
+加载背景音乐
+播放背景音乐
+我方飞机诞生
+interval = 0
+while Ture:
+	if 点击关闭:
+		退出
+		break;
+	interval += 1
+	if interval == 50
+		生成小飞机
+		小飞机向下移动
+		interval = 0
+	刷新屏幕
+
+	if 鼠标移动
+		我方飞机位置 = 鼠标位置
+		刷新屏幕
+	if 我方飞机与小飞机撞了
+		我方飞机挂掉，播放挂机音乐
+		修改我方飞机图案
+		打印‘Game Over’
+		停止音乐，最好淡出
+'''
