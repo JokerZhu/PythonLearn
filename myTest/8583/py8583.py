e5a5a6f39adae7a05a34570e6366e4c4652401f7
zhu <z_jing_wei_10@163.com>
2015-12-09 15:15:12 +0800

    提交8583模块测试程序

---
+++ myTest/8583/py8583.py
@@ -0,0 +1,69 @@
+#!/usr/local/bin/python2.7
+from ISO8583.ISO8583 import ISO8583
+from ISO8583.ISOErrors import *
+import socket
+import sys
+import traceback
+
+serverIP = '192.168.1.170'
+serverPort = 8383
+
+def PackLogonPackage():
+	PackLogon = ISO8583()
+	transation = "800"
+	PackLogon.setTransationType(transation) 
+	PackLogon.setBit(11,5768)
+	PackLogon.setBit(41,'16688013')
+	PackLogon.setBit(42,'000000000000001')
+	PackLogon.setBit(60,'4629003')
+	PackLogon.setBit(63,'01 ')
+	#print( PackLogon.showRawIso())
+	message = PackLogon.getNetworkISO()
+	return message
+	#print('message = ',message)
+	pass
+
+def Login():
+	#pack 8583
+	package = PackLogonPackage()
+
+	
+	#send to server
+	sendRecvMessage(package)
+	return 0 
+	pass
+
+def sendRecvMessage(message):
+	s = None
+	for res in socket.getaddrinfo(serverIP, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+		af, socktype, proto, canonname, sa = res
+		try:
+			s = socket.socket(af, socktype, proto)
+		except socket.error, msg:
+			s = None
+			continue
+		try:
+			s.connect(sa)
+		except socket.error, msg:
+			s.close()
+			s = None
+			continue
+		break
+	if s is None:
+		print ('Could not connect :(')
+		sys.exit(1)
+	print('sending message = [%s]' % message)
+	s.send(message)
+	ans = s.recv(2048)
+	print('recv message = [%s]' % ans)
+	s.close()
+	return 0
+
+if __name__ == '__main__':
+	print('starting..')
+	#login 
+	if Login() < 0:
+		print('login error ')
+		sys.exit(-1)
+
+
